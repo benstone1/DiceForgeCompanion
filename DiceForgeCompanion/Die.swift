3e0a9c378e9844310821cd0a7dfae171dd84f7bb
Ben Stone <benjamin.h.stone@gmail.com>
2020-12-12 22:30:04 -0500

    remove comments

--- DiceForgeCompanion/Die.swift
+++ DiceForgeCompanion/Die.swift
@@ -1,21 +1,9 @@
 import UIKit
 
 struct Die {
-//    let faceOne: DieFace
-//    let faceTwo: DieFace
-//    let faceThree: DieFace
-//    let faceFour: DieFace
-//    let faceFive: DieFace
-//    let faceSix: DieFace
     let faces: [DieFace]
     init(faces: [DieFace]) {
         self.faces = faces
-//        self.faceOne = faces[0]
-//        self.faceTwo = faces[1]
-//        self.faceThree = faces[2]
-//        self.faceFour = faces[3]
-//        self.faceFive = faces[4]
-//        self.faceSix = faces[5]
     }
     static let startingDieOne = Die(faces: [
         DieFace(image: UIImage(named: "oneGold")!, type: .gold(1)),
